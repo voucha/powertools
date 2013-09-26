524693799d944b38b34007902074400c5611733f
Ilham Rizqi Sasmita <irs@sandiloka.com>
2013-09-26 23:55:50 +0700

    New DataModule: DataTargetVoucha4

--- import/src/DataTargetVoucha4.dfm
+++ import/src/DataTargetVoucha4.dfm
@@ -2,4 +2,30 @@ object DataModule1: TDataModule1
   OldCreateOrder = False
   Height = 224
   Width = 397
+  object Connection: TZConnection
+    ControlsCodePage = cCP_UTF16
+    UTF8StringsAsWideField = True
+    Catalog = ''
+    HostName = ''
+    Port = 0
+    Database = ''
+    User = ''
+    Password = ''
+    Protocol = 'postgresql-9'
+    Left = 28
+    Top = 16
+  end
+  object ZQuery: TZQuery
+    Connection = Connection
+    Params = <>
+    Left = 28
+    Top = 76
+  end
+  object ZSql: TZSQLProcessor
+    Params = <>
+    Connection = Connection
+    Delimiter = ';'
+    Left = 28
+    Top = 132
+  end
 end
--- import/src/DataTargetVoucha4.pas
+++ import/src/DataTargetVoucha4.pas
@@ -3,14 +3,21 @@ unit DataTargetVoucha4;
 interface
 
 uses
-  System.SysUtils, System.Classes;
+  Customers,
+  System.SysUtils, System.Classes, ZAbstractConnection, ZConnection, Data.DB,
+  ZAbstractRODataset, ZAbstractDataset, ZDataset, ZSqlProcessor;
 
 type
   TDataModule1 = class(TDataModule)
+    Connection: TZConnection;
+    ZQuery: TZQuery;
+    ZSql: TZSQLProcessor;
   private
     { Private declarations }
   public
     { Public declarations }
+    function findCustomerIdByMsisdn(const Value: string): Integer;
+    function addCustomer(var Data:TCustomerRecord):Boolean;
   end;
 
 var
@@ -21,5 +28,44 @@ implementation
 {%CLASSGROUP 'Vcl.Controls.TControl'}
 
 {$R *.dfm}
+
+{ TDataModule1 }
+
+function TDataModule1.addCustomer(var Data: TCustomerRecord): Boolean;
+var
+  P: TZSQLProcessor;
+begin
+  try
+    P:=TZSQLProcessor.Create(nil);
+    P.Connection:=Connection;
+
+  finally
+    FreeAndNil(P);
+  end;
+end;
+
+function TDataModule1.findCustomerIdByMsisdn(const Value: string): Integer;
+var
+  CustomerID: Integer;
+  Query: TZQuery;
+begin
+  CustomerID:=0;
+  try
+    Query:=TZQuery.Create(nil);
+    Query.Connection:=Connection;
+    Query.SQL.Text:='SELECT customerid FROM customermsisdn WHERE msisdn=' +
+      ':msisdn;';
+    Query.FieldByName('msisdn').AsString:=Value;
+    Query.Open;
+    if Query.RecordCount > 0 then
+    begin
+      Query.First;
+      CustomerID:=Query.FieldByName('customerid').AsInteger;
+    end;
+    Query.Close;
+  finally
+    FreeAndNil(Query);
+  end;
+end;
 
 end.
